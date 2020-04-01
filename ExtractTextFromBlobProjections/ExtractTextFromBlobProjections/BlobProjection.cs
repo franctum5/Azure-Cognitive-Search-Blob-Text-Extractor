1f1a7ae7e18e9f6ec3db4e48700d4b168a1c425d
Francesco Tumiatti <franctum@microsoft.com>
2020-04-01 17:14:29 +0200

    first push

---
+++ ExtractTextFromBlobProjections/ExtractTextFromBlobProjections/BlobProjection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtractTextFromBlobProjections
+{
+    
+    public class BlobProjection
+    {
+        public string metadata_storage_name { get; set; }
+        public string merged_content { get; set; }
+        public string languageCode { get; set; }
+        public string translatedText { get; set; }
+    }
+
+    
+
+}
